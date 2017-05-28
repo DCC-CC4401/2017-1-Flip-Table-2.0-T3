d6d8626f9f6c2395d241153d02c0580ff0341bdc
Fabian601 <fabian696.g@gmail.com>
2017-05-28 19:37:01 -0400

    peddler can checkin, established status change with available times

--- showcase/views.py
+++ showcase/views.py
@@ -7,6 +7,8 @@ from django.views.generic import View
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 
+import datetime
+
 from django.http import JsonResponse
 
 from account.models import Client, Peddler, Established
@@ -35,17 +37,30 @@ def item_new(request):
 
 def showcase(request, seller_id):
     seller = User.objects.get(id=seller_id)
+    now = datetime.datetime.now(tz=datetime.timezone(datetime.timedelta(hours=-4))).time()
+    is_available = False
     try:
-        seller_profile = Peddler.objects.get(user=seller)
+        seller_profile = Established.objects.get(user=seller)
+        start = seller_profile.start
+        end = seller_profile.end
+        if start < now and now < end:
+            is_available = True
     except:
-        seller_profile = get_object_or_404(Established, user=seller)
+        seller_profile = get_object_or_404(Peddler, user=seller)
+
     isPeddler = seller_profile.__class__ == Peddler
 
     isClient = False
 
+    isPeddlerInHisProfile = False
+
     current_user = request.user
     if current_user.is_authenticated() and Client.objects.filter(user=current_user).exists():
         isClient = True
+
+    if current_user.is_authenticated() and Peddler.objects.filter(
+            user=current_user).exists() and current_user == seller:
+        isPeddlerInHisProfile = True
 
     isFavorite = False
     if not seller_profile.client_set.all().filter(id=current_user.id).first() is None:
@@ -66,7 +81,8 @@ def showcase(request, seller_id):
     dishes = seller.dish_set.all()
     context = {'dishes': dishes, 'seller': seller, 'isPeddler': isPeddler, 'seller_profile': seller_profile,
                'image': img,
-               'isClient': isClient, 'isFavorite': isFavorite, 'favorite': favorite}
+               'isClient': isClient, 'isFavorite': isFavorite, 'favorite': favorite,
+               'isAvailable': is_available, 'isPeddlerInHisProfile': isPeddlerInHisProfile}
 
     return render(request, 'showcase.html', context)
 
@@ -88,3 +104,21 @@ def favorite_seller(request, seller_id):
             client.f_established.add(seller)
     client.save()
     return HttpResponse(status=204)
+
+
+def checkin(request, seller_id):
+    seller = get_object_or_404(User, id=seller_id)
+    seller_profile = get_object_or_404(Peddler, user=seller)
+    if seller_profile.available:
+        seller_profile.available = False
+    else:
+        seller_profile.available = True
+    seller_profile.save()
+    return HttpResponse(status=204)
+
+# def peddler_set_not_available(request, seller_id):
+#     seller = get_object_or_404(User, id=seller_id)
+#     seller_profile = get_object_or_404(Peddler, user=seller)
+#     seller_profile.available = False
+#     seller_profile.save()
+#     return HttpResponse(status=204)
