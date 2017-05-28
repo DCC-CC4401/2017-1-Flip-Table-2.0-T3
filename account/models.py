7acf7d93e870c4803e1a58625ada0ec88318e034
elixs <elixs.777@gmail.com>
2017-05-28 03:10:14 -0400

    favorites working

--- account/models.py
+++ account/models.py
@@ -2,14 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Client(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField()
-
-    def __str__(self):
-        return "Client - " + self.user.username
-
-
 class Peddler(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField()
@@ -34,3 +26,13 @@ class Established(models.Model):
 
     def __str__(self):
         return "Established - " + self.user.username
+
+
+class Client(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField()
+    f_peddler = models.ManyToManyField(Peddler, blank=True)
+    f_established = models.ManyToManyField(Established, blank=True)
+
+    def __str__(self):
+        return "Client - " + self.user.username
--- static/js/init.js
+++ static/js/init.js
@@ -11,9 +11,21 @@
 
     });
 
+    $('#favorite').click(function () {
+        var fc = $('#favorite_count');
+        if ($(this).text() === 'star') {
 
+            $(this).html('star_border');
+            fc.html((parseInt(fc.text()) - 1).toString())
+        }
+        else {
+            $(this).html('star');
+            fc.html((parseInt(fc.text()) + 1).toString())
+        }
+    });
 
-})(jQuery);
+})
+(jQuery);
 
 
 
