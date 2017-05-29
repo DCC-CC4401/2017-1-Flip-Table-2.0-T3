f1767699d0a60a4bbaf9a85764a48ed16a5e403a
Diego Baño <diego.bano@ug.uchile.cl>
2017-05-28 20:41:51 -0400

    Added password changing forms

--- account/urls.py
+++ account/urls.py
@@ -33,9 +33,6 @@ urlpatterns = [
 
     url(r'^edit/established$', views.edit_established, name='edit_established'),
 
-    # # /account/login
-    # url(r'^login/$', views.login, name='login'),
-
     # /account/register
     url(r'^register/$', views.register, name='register'),
 
@@ -55,6 +52,16 @@ urlpatterns = [
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
 
     # /account/logout
-    url(r'^logout/$', auth_views.logout, {'next_page': '/account/login'}, name='logout')
+    url(r'^logout/$', auth_views.logout, {'next_page': '/account/login'}, name='logout'),
 
+    # /password*
+    url(r'^password_reset/$', auth_views.password_reset, {'template_name': 'password_reset_form.html'},
+        name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done, {'template_name': 'password_reset_done.html'},
+        name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm, {'template_name': 'password_reset_confirm.html'},
+        name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, {'template_name': 'password_reset_complete.html'},
+        name='password_reset_complete'),
 ]
